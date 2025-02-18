4bcb29e77b8398b2f7bb960bfed8044ad5b06292
Rahul Kumar Singh <singhrahulkumar820@gmail.com>
2025-02-18 12:26:48 +0530

    Time: 27 ms (86.38%), Space: 64.2 MB (27.51%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let map = new Map();
+    for(let word of strs){
+        let sortedWord = word.split("").sort().join("");
+        if(!map.has(sortedWord)){
+            map.set(sortedWord,[]);
+        }
+        map.get(sortedWord).push(word);
+
+    }
+    return Array.from(map.values())
+};
