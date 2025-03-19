0c3da05367e5f70b20e4f696d0b170f04e33f339
Rahul Kumar Singh <singhrahulkumar820@gmail.com>
2025-03-19 20:09:14 +0530

    Time: 56 ms (38.43%), Space: 53.5 MB (19.98%) - LeetHub

---
+++ 2762-cache-with-time-limit/2762-cache-with-time-limit.js
@@ -0,0 +1,27 @@
+var TimeLimitedCache = function() {
+    this.cache = new Map();
+};
+
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    const exists = this.cache.has(key);
+    if (exists) clearTimeout(this.cache.get(key).timeout);
+
+    const timeout = setTimeout(() => this.cache.delete(key), duration);
+    this.cache.set(key, { value, timeout });
+
+    return exists;
+};
+
+TimeLimitedCache.prototype.get = function(key) {
+    return this.cache.has(key) ? this.cache.get(key).value : -1;
+};
+
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
